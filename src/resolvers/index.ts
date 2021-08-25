5d35e2294c3fcb98dc8e35452240d3e68020aa54
Nathaniel Richards <nathaniel@datacompusa.com>
2021-08-25 00:46:33 -0400

    Added create user to root mutations

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,7 +1,11 @@
+import { userMutationResolvers } from './mutation/user';
 import { userQueryResolvers } from './query/user';
 
 export const resolvers: Resolvers.Resolvers = {
 	Query: {
 		...userQueryResolvers
+	},
+	Mutation: {
+		...userMutationResolvers
 	}
 };
