c6431f3df353f40728f50b573e90c0559d3f5b67
Nathaniel Richards <nathaniel@datacompusa.com>
2021-09-17 12:12:21 -0400

    FIxed password rest for accounts

--- src/resolvers/mutation/user/resetPassword.ts
+++ src/resolvers/mutation/user/resetPassword.ts
@@ -18,7 +18,7 @@ export const resetPassword: Resolvers.MutationResolvers['resetPassword'] =
 
 		await User.query()
 			.where({ email: existingCode.forEmail })
-			.patch({
+			.update({
 				password: await new AuthenticationService(
 					args.newCredentials.newPassword
 				).hashPassword()
